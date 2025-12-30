2b3dd6c5c25e310add9a4d972d4874c807b62199
YSChew <yschew@student.unimelb.edu.au>
2025-12-30 16:40:14 +1100

    Created requirements file for checking installed packages

--- TestApp.R
+++ TestApp.R
@@ -1,23 +1,13 @@
 source("src/server.R")
 source("src/ui.R")
-
+source("requirements/requirements.R")
 # ==============================================================================
 # SECTION 1: PACKAGE MANAGEMENT & SETUP
 # ==============================================================================
 
-# Define the list of required packages for the application
-packages <- c("shiny", "bslib", "DT", "dplyr", "readxl", "openxlsx", "tidyr", "rmarkdown", "shinyjs", "tinytex", "RColorBrewer")
-
-# Identify which packages are not yet installed on the system
-new_packages <- packages[!(packages %in% installed.packages()[,"Package"])]
-
-# Install missing packages if any are found
-if(length(new_packages)) install.packages(new_packages)
-
-# Ensure TinyTeX is installed for PDF generation immediately during setup
-if (!tinytex::is_tinytex()) {
-  tinytex::install_tinytex(force = TRUE)
-}
+# This is kept here for demonstration purposes, this should be removed after
+# it is hosted on WEHI servers and replaced with the check_requirements function
+run_setup()
 
 # Load libraries
 library(shiny)      # Core framework for the web app
