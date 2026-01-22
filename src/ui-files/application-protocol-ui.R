50abf79df2417a940b26f67469eb40814dea6329
yixuan6628-hash <yixuan6628@gmail.com>
2026-01-22 16:39:32 +1100

    feat(ui): update application and protocol selection layout

--- src/ui-files/application-protocol-ui.R
+++ src/ui-files/application-protocol-ui.R
@@ -4,13 +4,23 @@ structure_application_protocol_page <- function() {
   platform_panel <- nav_panel(
     title = "1. Select Application/Protocol",
     value = "tab_application",
-    card (
+    card(
       card_header("Select Application and Protocol"),
-      h3("Application Select"),
-      # Vertical button stack generated dynamically
-      uiOutput("application_button_ui"),
-      h3("Protocol Select"),
-      uiOutput("protocol_button_ui")
+      layout_column_wrap(
+        width = 1/2,
+        fixed_width = TRUE,
+        div(
+          style = "min-height: 1066px; padding: 20px; width: 150%;",
+          h3("Application Select", style = "color: rgba(31, 58, 147, 0.85); font-weight: bold; margin-bottom: 20px;"),
+          # Vertical button stack generated dynamically
+          uiOutput("application_button_ui")
+        ),
+        div(
+          style = "min-height: 1066px; padding: 20px; width: 150%;",
+          h3("Protocol Select", style = "color: rgba(31, 58, 147, 0.85); font-weight: bold; margin-bottom: 20px;"),
+          uiOutput("protocol_button_ui")
+        )
+      )
     )
   )
   
